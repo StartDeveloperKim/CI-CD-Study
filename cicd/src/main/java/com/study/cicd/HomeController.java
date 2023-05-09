0d188e622dce290f7505f52479f845355db3d075
StartDeveloperKim <ktw550111@naver.com>
2023-05-10 00:30:45 +0900

    Feat. 로그 수정

--- cicd/src/main/java/com/study/cicd/HomeController.java
+++ cicd/src/main/java/com/study/cicd/HomeController.java
@@ -1,11 +1,13 @@
 package com.study.cicd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("test")
 public class HomeController {
@@ -13,6 +15,7 @@ public class HomeController {
     @GetMapping("/{page}")
     public String test(@PathVariable(value = "page", required = false) Integer page, Model model) {
         model.addAttribute("page", page);
+        log.info("현재 페이지는 {} 입니다.", page);
         return "index";
     }
 }
