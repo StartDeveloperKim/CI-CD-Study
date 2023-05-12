681ebb784b83da0c2e6a3b0b09dd8cfed7b9cdff
StartDeveloperKim <ktw550111@naver.com>
2023-05-12 22:09:45 +0900

    스크립트 작성

--- src/main/java/com/study/cicd/CicdApplication.java
+++ src/main/java/com/study/cicd/CicdApplication.java
@@ -2,12 +2,15 @@ package com.study.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class CicdApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CicdApplication.class, args);
+		SpringApplication application = new SpringApplication(CicdApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
